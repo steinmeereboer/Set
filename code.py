c2594e57e740fcab91e1863a5677af7702578a5d
Fien van Berkel <40270109+fienvanberkel@users.noreply.github.com>
2018-06-19 11:06:41 +0200

    f

--- code.py
+++ code.py
@@ -1,3 +1,19 @@
+ovaal = 0
+rechthoek = 1
+golf = 2
+
+blauw = 0
+groen = 1
+roze = 2
+
+leeg = 0
+vol = 1
+gespikkeld = 2
+
+een = 0
+twee = 1
+drie = 2
+
 class set_kaart:
 
     def __init__(self, aantal = 0, vorm = 0, kleur = 0, vulling = 0):
@@ -5,6 +21,19 @@ class set_kaart:
         self.vorm = vorm
         self.kleur = kleur
         self.vulling = vulling
+
+    def derde_kaart(self, other):
+
+        kaart_3 == set_kaart()
+
+        if if (self.aantal + other.aantal)%3 == 0:
+            kaart_3.aantal == self.aantal + other.aantal
+        if (self.aantal + other.aantal)%3 == 1:
+            kaart_3.aantal == self.aantal + other.aantal + 2
+        if (self.aantal + other.aantal)%3 == 2:
+            kaart_3.aantal == self.aantal + other.aantal + 1
+
+
 
 
     def __add__ (self, other):
