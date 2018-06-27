fa47c5b5ca847ed901a56e6ca0d95d1f33fdf8ce
Fien van Berkel <40270109+fienvanberkel@users.noreply.github.com>
2018-06-27 16:17:28 +0200

    mooie naampjes

--- spel.py
+++ spel.py
@@ -5,16 +5,17 @@ import random
 def startspel():
     set_stapel = set.maakstapel()
     tafel = set.kaartenoptafel()
-    while len(set_stapel) > 2:
-        print(set.set_kaart.vind_alle_set(tafel))
-        print(tafel)
-        a = [int(i)  for i in input().split()]
-        if set.set_kaart.derde_kaart(tafel[a[0]], tafel[a[1]]) == tafel[a[2]]:
-            indices = a[0], a[1], a[2]
+    while len(set_stapel) != 0:
+        speler_invoer = [int(i) for i in input().split()]
+        if (set.set_kaart.derde_kaart(tafel[speler_invoer[0]],
+            tafel[speler_invoer[1]]) == tafel[speler_invoer[2]]):
+            indices = speler_invoer[0], speler_invoer[1], speler_invoer[2]
             tafel = [i for j, i in enumerate(tafel) if j not in indices]
+
             for i in range(3):
-                a = set_stapel.pop(random.randrange(0,len(set_stapel)))
-                tafel.append(a)
+                nieuwe_kaart = set_stapel.pop(random.randrange(0,len(set_stapel)))
+                tafel.append(nieuwe_kaart)
+
         print(tafel)
     return tafel
 
