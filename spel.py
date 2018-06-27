d718f9245ddb3c68b7f511b16c18b9ec2258c737
Olffie <40270826+Olffie@users.noreply.github.com>
2018-06-27 15:52:58 +0200

    push

--- spel.py
+++ spel.py
@@ -5,15 +5,22 @@ import random
 def startspel():
     set_stapel = set.maakstapel()
     tafel = set.kaartenoptafel()
-    print(set.set_kaart.vind_alle_set(tafel))
-    print(tafel)
-    a = [int(i)  for i in input().split()]
-    if set.set_kaart.derde_kaart(tafel[a[0]], tafel[a[1]]) == tafel[a[2]]:
-        indices = a[0], a[1], a[2]
-        tafel = [i for j, i in enumerate(tafel) if j not in indices]
-        for i in range(3):
-            a = set.set_stapel.pop(random.randrange(0,len(set_stapel)))
-            tafel.append(a)
+    while len(set_stapel) > 2:
+        print(set.set_kaart.vind_alle_set(tafel))
+        print(tafel)
+        a = [int(i)  for i in input().split()]
+        if set.set_kaart.derde_kaart(tafel[a[0]], tafel[a[1]]) == tafel[a[2]]:
+            indices = a[0], a[1], a[2]
+            tafel = [i for j, i in enumerate(tafel) if j not in indices]
+            for i in range(3):
+                a = set.set_stapel.pop(random.randrange(0,len(set_stapel)))
+                tafel.append(a)
+        print(tafel)
     return tafel
 
-print(startspel())
+set_stapel = set.maakstapel()
+
+print(len(set_stapel))
+for i in range(3):
+    a = set_stapel.pop(random.randrange(0,len(set_stapel)))
+print(len(set_stapel))
