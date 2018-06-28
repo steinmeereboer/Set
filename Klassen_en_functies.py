8a82e0e8fd1d0ef74a139bf798fd51847d2b6377
Fien van Berkel <40270109+fienvanberkel@users.noreply.github.com>
2018-06-28 16:58:47 +0200

    mooi man

--- Klassen_en_functies.py
+++ Klassen_en_functies.py
@@ -1,11 +1,17 @@
-import random
+'''Uitleg van dit document'''
 
-ovaal, rechthoek, golf = range(3)
-blauw, groen, roze = range(3)
-leeg, vol, gestreept = range(3)
-een, twee, drie = range(3)
+# pylint: disable = C0103
+
+import random
+from itertools import combinations, product
+
+OVAAL, RECHTHOEK, GOLF = range(3)
+BLAUW, GROEN, ROZE = range(3)
+LEEG, VOL, GESTREEPT = range(3)
+EEN, TWEE, DRIE = range(3)
 
 class set_kaart:
+    '''Uitleg van de klasse'''
 
     def __eq__(self, other):
         return (self.aantal == other.aantal and self.vorm == other.vorm
@@ -16,66 +22,52 @@ class set_kaart:
                 f'(aantal={self.aantal},vorm={self.vorm},'
                 f'kleur={self.kleur},vulling={self.vulling})')
 
-    def __init__(self, aantal = 0, vorm = 0, kleur = 0, vulling = 0):
+    def __init__(self, aantal=0, vorm=0, kleur=0, vulling=0):
         self.aantal = aantal
         self.vorm = vorm
         self.kleur = kleur
         self.vulling = vulling
 
-    #Functie die bij twee setkaarten een derde kaart vindt zodat ze samen een set vormen
     def derde_kaart(self, other):
+        '''Functie die bij twee setkaarten een derde kaart vindt zodat ze samen een set vormen'''
 
         kaart_3 = set_kaart()
 
-        #Functie die de eigenschappen van de derde kaart bepaalt
-        def eigenschap_kaart(set_kaart, eigenschap, eigenschap_1, eigenschap_2):
-            if (eigenschap_1 + eigenschap_2)%3 == 0:
-                setkaart.eigenschap = 0
-            if (eigenschap_1 + eigenschap_2)%3 == 1:
-                setkaart.eigenschap = 2
-            if (eigenschap_1 + eigenschap_2)%3 == 2:
-                setkaart.eigenschap = 1
-            return set_kaart.eigenschap
+        def eigenschap_kaart(eigenschap_1, eigenschap_2):
+            '''Functie die de eigenschappen van de derde kaart bepaalt'''
+            lijst = [0, 2, 1]
+            return lijst[(eigenschap_1 + eigenschap_2)%3]
 
-        kaart_3.aantal = eigenschap_kaart(kaart_3, 'aantal', self.aantal, other.aantal)
-        kaart_3.vorm = eigenschap_kaart(kaart_3, 'vorm', self.vorm, other.vorm)
-        kaart_3.kleur = eigenschap_kaart(kaart_3, 'kleur', self.kleur, other.kleur)
-        kaart_3.vulling = eigenschap_kaart(kaart_3, 'vulling', self.vulling, other.vulling)
+        kaart_3.aantal = eigenschap_kaart(self.aantal, other.aantal)
+        kaart_3.vorm = eigenschap_kaart(self.vorm, other.vorm)
+        kaart_3.kleur = eigenschap_kaart(self.kleur, other.kleur)
+        kaart_3.vulling = eigenschap_kaart(self.vulling, other.vulling)
 
         return kaart_3
 
-    #Functie die een set vindt in een lijst van setkaarten
-    def vind_set(lijst):
-        for kaart_1 in lijst:
-            for kaart_2 in lijst:
-                if kaart_2 != kaart_1:
-                    for kaart_3 in lijst:
-                        if  kaart_3 == set_kaart.derde_kaart(kaart_1, kaart_2):
-                            return([kaart_1, kaart_2, kaart_3])
-                            break
+def vind_set(lijst):
+    '''Functie die een set vindt in een lijst van setkaarten'''
+    for kaart_1, kaart_2 in combinations(lijst, 2):
+        kaart_3 = set_kaart.derde_kaart(kaart_1, kaart_2)
+        if kaart_3 in lijst:
+            return [kaart_1, kaart_2, kaart_3]
+    return None
 
-    #Functie die alle sets vindt in een lijst van setkaarten
-    def vind_alle_sets(lijst):
-        alle_sets = []
-        for kaart_1 in lijst:
-            for kaart_2 in lijst:
-                if kaart_2 != kaart_1:
-                    for kaart_3 in lijst:
-                        if  kaart_3 == set_kaart.derde_kaart(kaart_1, kaart_2):
-                            alle_sets.append([kaart_1, kaart_2, kaart_3])
-        return(alle_sets)
+def vind_alle_sets(lijst):
+    '''Functie die alle sets vindt in een lijst van setkaarten'''
+    alle_sets = set()
+    for kaart_1, kaart_2 in combinations(lijst, 2):
+        kaart_3 = set_kaart.derde_kaart(kaart_1, kaart_2)
+        if kaart_3 in lijst:
+            alle_sets.add({kaart_1, kaart_2, kaart_3})
+    return alle_sets
 
-#Functie die de 81 unieke setkaarten maakt en hier een stapel van maakt
 def maak_stapel():
+    '''Functie die de 81 unieke setkaarten maakt en hier een stapel van maakt'''
     set_stapel = []
-    for i in range (3):
-        for j in range (3):
-            for k in range (3):
-                for l in range (3):
-                    set_stapel.append(set_kaart(i,j,k,l))
-    return(set_stapel)
-
-set_stapel = maak_stapel()
+    for i, j, k, l in product(range(3), range(3), range(3), range(3)):
+        set_stapel.append(set_kaart(i, j, k, l))
+    return set_stapel
 
 #Functie die 12 willekeurige kaarten uit de stapel kiest en toevoegt aan de kaarten op tafel
 def kaarten_op_tafel():
@@ -91,3 +83,8 @@ def zie_tafel():
     print(b[3:6])
     print(b[6:9])
     print(b[9:12])
+
+
+
+a = set_kaart()
+print(eigenschap_kaart(a, 'kleur', ROZE, BLAUW))
--- spel.py
+++ spel.py
@@ -1,18 +1,21 @@
-import Klassen_en_functies as set
+''''''
+
 import random
 import time
+import klassen_en_functies as klasse
 
 def start_spel():
+    '''Deze functie is het hele spel'''
     punten_speler = 0
     punten_computer = 0
-    set_stapel = set.maak_stapel()
-    tafel = set.kaarten_op_tafel()
+    set_stapel = klasse.maak_stapel()
+    tafel = klasse.kaarten_op_tafel()
     while len(set_stapel) > 2:
         print(tafel)
         start = time.perf_counter()
         if time.perf_counter() - start < 5:
             speler_invoer = [int(i)  for i in input().split()]
-            if (set.set_kaart.derde_kaart(tafel[speler_invoer[0]],
+            if (klasse.set_kaart.derde_kaart(tafel[speler_invoer[0]],
             tafel[speler_invoer[1]]) == tafel[speler_invoer[2]]):
                 indices = speler_invoer[0], speler_invoer[1], speler_invoer[2]
                 tafel = [i for j, i in enumerate(tafel) if j not in indices]
@@ -22,7 +25,7 @@ def start_spel():
                     nieuwe_kaart = set_stapel.pop(random.randrange(0,len(set_stapel)))
                     tafel.append(nieuwe_kaart)
 
-        if set.set_kaart.vind_alle_sets(tafel) == []:
+        if klasse.set_kaart.vind_alle_sets(tafel) == []:
                 indices = 0, 1, 2
                 tafel = [i for j, i in enumerate(tafel) if j not in indices]
                 for i in range(3):
@@ -30,8 +33,8 @@ def start_spel():
                     tafel.append(nieuwe_kaart)
 
         elif (time.perf_counter() - start >= 5 or
-        set.set_kaart.derde_kaart(tafel[speler_invoer[0]], tafel[a[1]]) != tafel[a[2]]):
-            sets = set.set_kaart.vind_alle_sets(tafel)
+              klasse.set_kaart.derde_kaart(tafel[speler_invoer[0]], tafel[a[1]]) != tafel[a[2]]):
+            sets = klasse.set_kaart.vind_alle_sets(tafel)
             drie_kaarten = random.choice(sets)
             tafel.remove(drie_kaarten[2])
             tafel.remove(drie_kaarten[1])
